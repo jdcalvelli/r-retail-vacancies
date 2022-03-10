65f53c1e31347ff97b9190f6c25419c51487bdfc
JD Calvelli <jdcalvelli@gmail.com>
2022-03-10 12:13:06 -0600

    integrating leaflet and linking to ui options

--- RVEXShiny/app.R
+++ RVEXShiny/app.R
@@ -39,7 +39,7 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           textOutput('leaf')
+           leafletOutput('leaf')
         )
     )
 )
@@ -49,11 +49,86 @@ server <- function(input, output) {
   
   #saves a function you have to call in a render elsewhere
   re <- eventReactive(input$submit, {
-    paste(input$zipCode, input$keyWord, input$searchingRadius)
+    # ZIP CODE TO BOUNDING BOX API CALL
+    ## get bounding box based on zip code - text search
+    zipCodeAPICall <- GET('https://maps.googleapis.com/maps/api/place/textsearch/json',
+                          query = list(query = input$zipCode,
+                                       key = API_KEY))
+    
+    zipCodeAPICallResults <- fromJSON(rawToChar(zipCodeAPICall$content))$results
+    
+    boundingBox <- list(NE_Lat = zipCodeAPICallResults$geometry$viewport$northeast$lat,
+                        NE_Lng = zipCodeAPICallResults$geometry$viewport$northeast$lng,
+                        SW_Lat = zipCodeAPICallResults$geometry$viewport$southwest$lat,
+                        SW_Lng = zipCodeAPICallResults$geometry$viewport$southwest$lng)
+    
+    
+    # BOUNDING BOX TO COORDINATES FCN
+    ## create set of coordinates based on the bounding box
+    DESIRED_GRID_LENGTH = 3
+    INTERMEDIATE_GRID_LENGTH = DESIRED_GRID_LENGTH - 1
+    LAT_STEP_SIZE = (boundingBox$NE_Lat - boundingBox$SW_Lat) / INTERMEDIATE_GRID_LENGTH
+    LNG_STEP_SIZE = (boundingBox$NE_Lng - boundingBox$SW_Lng) / INTERMEDIATE_GRID_LENGTH
+    
+    coordinatesVector = c()
+    
+    lat = boundingBox$SW_Lat
+    while(lat <= boundingBox$NE_Lat) {
+      lng = boundingBox$SW_Lng
+      while(lng <= boundingBox$NE_Lng) {
+        coordinatesVector = append(coordinatesVector, paste(lat, lng, sep=" "))
+        lng = lng + LNG_STEP_SIZE
+      }
+      lat = lat + LAT_STEP_SIZE
+    }
+    
+    # COORDINATES + KEYWORD TO BUSINESSES API CALL
+    # run a search for each coordinate and combine results
+    # first we need to create a data frame to join all the others to
+    establishmentsAppended <- data.table()
+    
+    # then we need to make the queries and join them to the final dataframe WO PAGES
+    # for in R is like forEach array method in js
+    for(coordinate in coordinatesVector) {
+      establishmentsAPICall <- GET('https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+                                   query = list(keyword = input$keyWord,
+                                                location = coordinate,
+                                                radius = input$searchingRadius,
+                                                key = API_KEY))
+      establishmentsAPICallResults <- fromJSON(rawToChar(establishmentsAPICall$content),
+                                               flatten = TRUE)$results
+      setDT(establishmentsAPICallResults)
+      
+      establishmentsAppended <- rbind(establishmentsAppended, establishmentsAPICallResults, fill=TRUE)
+    }
+    
+    # FILTERING ESTABLISHMENTSDF TO ONLY TEMP/PERM CLOSED
+    
+    # keep only the temp/perm closed establishments
+    ESTTEMP <- filter(establishmentsAppended, business_status == 'CLOSED_TEMPORARILY')
+    # remove any and all duplicates
+    ESTTEMP <- distinct(ESTTEMP)
+    
+    # MAP MAKING
+    
+    leaf <- leaflet(data = ESTTEMP) %>%
+      setView(lng = zipCodeAPICallResults$geometry$location$lng,
+              lat = zipCodeAPICallResults$geometry$location$lat,
+              zoom = 12) %>%
+      addTiles() %>% 
+      addMarkers(lng = ~geometry.location.lng,
+                 lat = ~geometry.location.lat,
+                 popup = sprintf(
+                   '<h3> %s </h3>
+               <p> %s </p>',
+                   ESTTEMP$name,
+                   ESTTEMP$business_status) %>% lapply(htmltools::HTML)
+      )
+    leaf
   })
   
   #actually calling the render and saving it to the output
-  output$leaf <- renderText({
+  output$leaf <- renderLeaflet({
     re()
   })
   
